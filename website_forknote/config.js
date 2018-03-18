e94f2fd5b798d902e410a9ade048916f9f1b80d4
ridd <24556120+ridd84@users.noreply.github.com>
2018-03-18 20:07:45 +0100

    Upload basic forknote website

---
+++ website_forknote/config.js
@@ -0,0 +1,35 @@
+var api = "http://poolhost.com:8118";
+
+var api_blockexplorer = "http://daemonhost.com:46348";
+
+var poolHost = "poolhost.com";
+
+var irc = "irc.freenode.net/#poolhost";
+
+var email = "support@poolhost.com";
+
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://178.22.71.122/?hash={id}#blockchain_block";
+
+var transactionExplorer = "http://178.22.71.122/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default-theme.css";
+
+var networkStat = {
+    "croat": [
+        ["croat.mpool.us", "http://125.212.245.165:8117"],
+        ["croat.xeminer.net", "http://croat.xeminer.net:8117"],
+        ["croat.ernico.es", "http://croat.ernico.es:8089"],
+        ["croat.fastpool.eu", "http://croat.fastpool.eu:8117"],
+        ["croat.pool.cat", "http://croat.pool.cat:8119"],
+        ["pool.croat.cat", "http://178.22.66.126:8117"],
+        ["croat.netweekend.org", "http://croat.netweekend.org:1117"],
+        ["croat.netips.net", "http://croat.netips.net:8117"],
+        ["croat.globalpool.cc", "http://111.231.72.116:8103"],
+        ["croat.pools.e-scavo.net.ar", "http://croat.pools.e-scavo.net.ar:8990"],
+        ["165.227.148.241", "http://165.227.148.241:8117"]
+    ]
+};
